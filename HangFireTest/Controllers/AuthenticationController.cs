615856ae0b5181008284d7837ca5e3f2c5613540
Onur DİNÇ <103895148+onrdinc@users.noreply.github.com>
2023-12-04 09:41:52 +0300

    .

--- HangFireTest/Controllers/AuthenticationController.cs
+++ HangFireTest/Controllers/AuthenticationController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HangFireTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
 
 namespace HangFireTest.Controllers
 {
 	public class AuthenticationController : Controller
 	{
+        [HttpGet]
 		public IActionResult Index()
 		{
 			return View();
-		}
-	}
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogIn(LoginDto dto)
+        {
+            if (dto.UserName == "Admin" && dto.Password == "123456")
+            {
+                string serializedData = JsonSerializer.Serialize(true);
+                byte[] byteArray = Encoding.UTF8.GetBytes(serializedData);
+                HttpContext.Session.Set("hangfireAdmin", byteArray);
+                return Redirect("/hangfire"); 
+            }
+            else
+            {
+                return Unauthorized(); 
+            }
+        }
+
+
+
+    }
 }
